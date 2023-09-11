0ebb53ea93ab1270c1262f7fd46672544c057b90
Sudip Pawar <127401315+Sudippawar89@users.noreply.github.com>
2023-09-11 20:25:06 +0530

    Add files via upload

---
+++ 2.Budget_Calculator/budget_calculator.sql
@@ -0,0 +1,17 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+CREATE TABLE `budget` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `amount` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+ALTER TABLE `budget`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `budget`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
